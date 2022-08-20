c06d6a05142bb537d782c6f04fcc6084978843a5
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-08-20 06:24:17 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import About from './Pages/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <button class="btn">Button</button>
+    <div>
+     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+    </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#0FCFEC",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
