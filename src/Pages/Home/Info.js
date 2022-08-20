4892c112f2b8aac436da2a1185a503d357d84ac6
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-08-20 07:52:23 +0600

    info section

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg'
+import phone from '../../assets/icons/phone.svg'
+import marker from '../../assets/icons/marker.svg'
+
+const Info = () => {
+     return (
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+               <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" text="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
+               <InfoCard bgClass="bg-primary" text="Brooklyn, NY 10036, United States" cardTitle="Visit our location" img={marker}></InfoCard>
+               <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" text="+000 123 456789" cardTitle="Contact us now" img={phone}></InfoCard>
+          </div>
+     );
+};
+
+export default Info;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         mytheme: {
           primary: "#0FCFEC",
           secondary: "#19D3AE",
-          accent: "#0FCFEC",
+          accent: "#0D544A",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
